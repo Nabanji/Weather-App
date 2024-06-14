501637d68f3ca3ae0b6f5693f8d07cb8d351b300
Prince Nabanji <pnabanji@gmail.com>
2024-06-14 19:06:30 +0300

    Complete functionality

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ let cityName = document.getElementById("city-name");
 const submitBtn = document.getElementById("submit-btn");
 
 const apiKey = "6a645c553fb6093acf2163b101c73078";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+
+async function checkWeather(city) {
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
 
+
+    let value = document.getElementById("value");
+    value.textContent = data.name;
+
+    let temperature = document.getElementById("temperature");
+    let tempValue = Math.round(data.main.temp);
+    temperature.textContent = tempValue + "°C";
+
+    let humidity = document.getElementById("humidity");
+    humidity.textContent = data.main.humidity + "%";
+
+    let windSpeed = document.getElementById("wind-speed");
+    let windSpeedValue = data.wind.speed;
+    windSpeed.textContent = windSpeedValue + "km/h";
+
     console.log(data);
-    // console.log(cityName.value);
-    // e.preventDefault();
-    // cityName.value = "";
 }
 
-checkWeather();
+submitBtn.addEventListener("click", () => {
+    checkWeather(cityName.value);
+})
 
-// submitBtn.addEventListener("click", displayCity);
+// checkWeather();
